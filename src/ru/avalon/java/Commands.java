2af99dafa2ced4ae5324abbc0e1040f4ba71f0c2
Vlasov Sergey <servla@WS-248-10.avalon.ru>
2020-01-13 20:57:03 +0300

    Власов С. / DEV-J130 Лабораторная работа №3, занятие в классе 13.01.2020

--- src/ru/avalon/java/Commands.java
+++ src/ru/avalon/java/Commands.java
@@ -1,13 +1,39 @@
 package ru.avalon.java;
 
+import java.util.Arrays;
+
 /**
  * Обрабатываемые приложением команды.
  */
-public enum Commands {
+public class Commands {
+    public enum Command {
     move,
     copy,
     exit
-    /*
-     * TODO №8 К текущему списку команд, добавьте ещё две команды
-     */
+    }
+
+    public Command comm;
+    public String[] param;
+
+    public Commands(String input) throws IllegalCommand {
+        String[] parts = input.trim().split(" +");
+        switch (parts[0]) {
+            case "copy" : comm = Command.copy; break;
+            case "move" : comm = Command.move; break;
+            case "exit" : comm = Command.exit; break;
+            default :
+                throw new IllegalCommand("Не удалось распознать команду " + parts[0]);
+        }
+        param = Arrays.copyOfRange(parts, 1, parts.length); 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(comm.toString() + " Параметры: ");
+                for(String s: param){
+                    sb.append(" ").append(s);
+                }
+                return sb.toString();
+    }
+    
 }
--- src/ru/avalon/java/Lab3.java
+++ src/ru/avalon/java/Lab3.java
@@ -3,6 +3,10 @@ package ru.avalon.java;
 import ru.avalon.java.console.ConsoleUI;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ru.avalon.java.actions.FileCopyAction;
+import ru.avalon.java.actions.FileMoveAction;
 
 /**
  * Лабораторная работа №3
@@ -14,13 +18,22 @@ import java.io.IOException;
  *
  * @author Daniel Alpatov <danial.alpatov@gmail.com>
  */
-public class Lab3 extends ConsoleUI<Commands> {
+public class Lab3 extends ConsoleUI {
     /**
      * Точка входа в приложение.
      * 
      * @param args 
      */
     public static void main(String[] args) {
+//        try {
+//            
+//            //new FileCopyAction("README.md", "README2.md").start();
+//            //new FileMoveAction("README2.md", "README5.md").start();
+//            Commands cmd = new Commands("copy README.md README3.md");
+//            System.out.println("Команда " + cmd.toString() + " ");
+//        } catch (IllegalCommand ex) {
+//            System.out.println("Ошибка: " + ex.getMessage());
+//        }
         new Lab3().run();
     }
     /**
@@ -30,28 +43,28 @@ public class Lab3 extends ConsoleUI<Commands> {
      * перечисления {@link Commands}.
      */
     Lab3() {
-        super(Commands.class);
+//        super(Commands.class);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    protected void onCommand(Commands command) throws IOException {
-        switch (command) {
+    protected void onCommand(Commands command) {
+        switch (command.comm) {
             case copy:
-                /*
-                 * TODO №6 Обработайте команду copy
-                 */
+                new FileCopyAction(command.param[0], command.param[1]).start();
+                System.out.println("onCommand отработал для copy");
                 break;
             case move:
-                /*
-                 * TODO №7 Обработайте команду move
-                 */
+                new FileMoveAction(command.param[0], command.param[1]).start();
+                System.out.println("onCommand отработал для move");
                 break;
             case exit:
                 close();
                 break;
+            default:
+                System.out.println("Плохая команда");
                 /*
                  * TODO №9 Обработайте необработанные команды
                  */
